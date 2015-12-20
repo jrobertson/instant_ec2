9d84933444fecebe2d157bb634adca9a89276d14
Your Name <you@example.com>
2015-12-20 14:59:31 +0000

    feature: An EC2 instance can now be instructed to run for a duration specified in minutes.

--- lib/instant_ec2.rb
+++ lib/instant_ec2.rb
@@ -7,18 +7,25 @@ require 'aws-sdk'
 
 class EC2Instance < Hash
 
-  def initialize(h, ec2=nil)
+  def initialize(h, caller=nil)
 
-    @ec2 = ec2
+    @c = caller
     super().merge!(h)
 
   end
 
-  def start()
+  def start(duration: nil)
 
-    @ec2.start_instances instance_ids: [self[:instance_id]]
+    @c.start_instance self[:instance_id]       
+    Thread.new{ sleep duration * 60; self.stop} if duration
 
   end
+  
+  def stop()
+
+    @c.stop_instance self[:instance_id]
+
+  end  
 end
 
 class InstantEC2
@@ -39,9 +46,16 @@ class InstantEC2
     @images = image_names.zip(instance_ids).inject([]) do |r, x|
 
       name, id = x
-      r << EC2Instance.new({image_name: name, instance_id: id}, @ec2)
+      r << EC2Instance.new({image_name: name, instance_id: id}, self)
       
     end
+    
+    @hooks = {
+      running: ->(ip){ 
+        puts "%s: the instance is now accessible from %s" % [Time.now, ip]
+      },
+      stopping: ->(){ puts "%s: the instance is now stopping" % Time.now}
+    }
 
   end
 
@@ -62,49 +76,79 @@ class InstantEC2
     r.instances[0].public_ip_address if r
   end
   
-  def on_running()
-    
-    # timeout after 30 seconds
-    t1 = Time.now
-    sleep 1; ip = self.ip until ip or Time.now > t1 + 30
-    
-    if ip then 
-      yield(ip) 
-    else
-      puts 'on_running timedout'
-    end
-    
+  def on_running(&blk)
+    @hooks[:running] = blk
+  end    
+  
+  def running?
+    self.ip ? true : false
   end
   
-  alias running find_running
-
-  def start(s)
-    self.find_image(s).start
+  # Launch an EC2 instance. Duration (optional) specified in minutes
+  #
+  def start(s, duration: nil)
+    
+    self.find_image(s).start duration: duration
+    
   end
 
-  def stop
+  def start_instance(id)
+    @ec2.start_instances instance_ids: [id]
+    Thread.new { trigger_on_start() }
+  end
+  
+  def stopping
 
     r = self.find_running()
 
     if r then
       
       puts 'stopping ...'
-      instance_id = r.instances[0].instance_id      
-      @ec2.stop_instances instance_ids: [instance_id]
+      
+      self.stop_instance r.instances[0].instance_id      
       
     else
       puts 'no instances to stop'
     end
   end
   
-  def on_stopping()
+  def stop_instance(id)
+    @ec2.stop_instances instance_ids: [id]
+    trigger_on_stopping()    
+  end
+
+  def stopped?()
+    self.ip.nil?
+  end
+  
+  def on_stopped(&blk)
+    @hooks[:stopped] = blk
+  end  
+  
+  private
+  
+  def trigger_on_start()
+    
+    # timeout after 60 seconds
+    t1 = Time.now
+    sleep 1; ip = self.ip until ip or Time.now > t1 + 60
+    
+    if ip then 
+      @hooks[:running].call(ip)
+    else
+      puts 'on_running timedout'
+    end
+    
+  end
+  
+  def trigger_on_stopping()
     
     # timeout after 30 seconds
     t1 = Time.now
     sleep 1; ip = self.ip until ip.nil? or Time.now > t1 + 30
     
     if ip.nil? then 
-      yield
+      @hooks[:stopping].call()
     else
       puts 'on_stopping timedout'
     end
